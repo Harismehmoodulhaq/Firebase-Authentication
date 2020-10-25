9afe8412157b820f0707972416a4cb0e3dd6c957
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2020-10-25 18:55:09 +0500

    authentication

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDe2VA_jhRRy2AauZgmK3Eu7lhcAIm3f6E",
+  authDomain: "fire-authentication-angular.firebaseapp.com",
+  databaseURL: "https://fire-authentication-angular.firebaseio.com",
+  projectId: "fire-authentication-angular",
+  storageBucket: "fire-authentication-angular.appspot.com",
+  messagingSenderId: "684711368440"
+  }
+};
